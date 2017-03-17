3f2ff80ff2b3d0dbeec6424ecfa000f3b343ab8c
Long-Huei Chen <longhuei@stanford.edu>
2017-03-17 11:12:45 -0700

    checkout kshitiz-pa4 squad_preprocess

--- qa_data.py
+++ qa_data.py
@@ -162,7 +162,7 @@ if __name__ == '__main__':
     # ======== Trim Distributed Word Representation =======
     # If you use other word representations, you should change the code below
 
-    process_glove(args, rev_vocab, args.source_dir + "/glove.trimmed.{}".format(args.glove_dim),
+    process_glove(args, rev_vocab, args.source_dir + "/glove.trimmed.{}.{}".format(args.glove_crawl_size, args.glove_dim),
                   random_init=args.random_init)
 
     # ======== Creating Dataset =========
