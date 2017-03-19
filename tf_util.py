0feaafb4b212808c7ef11fb4d2db54c1ee2a6cc9
Kshitiz Tripathi <tripathi@yahoo-inc.com>
2017-03-18 22:20:53 -0700

    update to tf_util

--- tf_util.py
+++ tf_util.py
@@ -2,14 +2,17 @@ import tensorflow as tf
 import numpy as np
 
 def _3d_X_2d(_3d_tensor, _2d_tensor):
+    shape_3d = _3d_tensor.get_shape().as_list()
+    shape_2d = _2d_tensor.get_shape().as_list()
+
     return tf.reshape(
         tf.matmul(
             tf.reshape(
                 _3d_tensor,
-                [-1, tf.shape(_2d_tensor)[0]]
+                [-1, shape_2d[0]]
             ),
-        _2d_tensor ),
-        [-1, tf.shape(_3d_tensor)[1], tf.shape(_3d_tensor)[2]]
+        _2d_tensor),
+        [-1, shape_3d[1], shape_3d[2]]
     )
 
 def test_3d_X_2d():
@@ -18,7 +21,7 @@ def test_3d_X_2d():
     with tf.Session() as session:
         a = session.run(tf.map_fn(lambda x: tf.matmul(x, W), L))
         b = session.run(_3d_X_2d(L,W))
-        print np.all(a==b)
+        assert np.all(a==b)
 
 def assert_shape(var, var_name, expected):
     shape = var.get_shape().as_list()
